81c4431451a366a6ab12c1870ca5a67a559c3cec
Nick Wade <nwade@godaddy.com>
2022-01-04 14:33:53 -0600

    Add dev tools for the interim

--- config/config.fish
+++ config/config.fish
@@ -1,5 +1,22 @@
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -g theme_display_git_default_branch yes
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
 gpg-connect-agent /bye
 #set -g theme_git_default_branches master main
 #set -g theme_git_worktree_support yes
+set -g theme_display_k8s_context yes
+set -g theme_display_k8s_namespace yes
+
+if test -e ~/.asdf/asdf.fish
+    source ~/.asdf/asdf.fish
+    if ! test -e ~/.asdf/completions/asdf.fish
+        mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+    end
+    if ! test -e ~/.tool-versions
+        asdf plugin add python
+        asdf install python 3.10.1
+        asdf global python 3.10.1
+    end
+end
+
+direnv hook fish | source
--- ncwade.nix
+++ ncwade.nix
@@ -176,6 +176,12 @@ in
 
     xdg.configFile."kdeglobals".source = ./config/kdeglobals;
 
+    home.sessionVariables = {
+      CONFIGURE_OPTS="--with-openssl=$(nix eval --raw nixpkgs.openssl.dev.outPath)";
+      CPPFLAGS="-I$(nix eval --raw nixpkgs.zlib.dev.outPath)/include -I$(nix eval --raw nixpkgs.libffi.dev.outPath)/include -I$(nix eval --raw nixpkgs.openssl.dev.outPath)/include -I$(nix eval --raw nixpkgs.readline.dev.outPath)/include -I$(nix eval --raw nixpkgs.ncurses.dev.outPath)/include -I$(nix eval --raw nixpkgs.expat.dev.outPath)/include -I$(nix eval --raw nixpkgs.sqlite.dev.outPath)/include -I$(nix eval --raw nixpkgs.bzip2.dev.outPath)/include";
+      LDFLAGS="-L$(nix eval --raw nixpkgs.zlib.out)/lib -L$(nix eval --raw nixpkgs.libffi.out)/lib -L$(nix eval --raw nixpkgs.openssl.out)/lib -L$(nix eval --raw nixpkgs.readline.outPath)/lib -L$(nix eval --raw nixpkgs.ncurses.out)/lib -L$(nix eval --raw nixpkgs.expat.outPath)/lib -L$(nix eval --raw nixpkgs.sqlite.out)/lib -L$(nix eval --raw nixpkgs.bzip2.out)/lib";
+    };
+    programs.go.enable = true;
     home.packages = [
       pkgs.kitty
       pkgs.fzf
@@ -186,6 +192,26 @@ in
       pkgs.fd
       pkgs.python3
       pkgs.ripgrep
+      pkgs.kubectl
+      pkgs.poetry
+      pkgs.python3Packages.pipx
+      pkgs.gnumake
+      pkgs.binutils
+      pkgs.zlib.dev
+      pkgs.libffi.dev
+      pkgs.openssl.dev
+      pkgs.readline.dev
+      pkgs.ncurses.dev
+      pkgs.expat.dev
+      pkgs.sqlite.dev
+      pkgs.bzip2.dev
+      pkgs.xz
+      pkgs.gcc
+      pkgs.nodejs
+      pkgs.docker-compose
+      pkgs.libcap
+      pkgs.pre-commit
+      pkgs.direnv
     ];
   };
 }
