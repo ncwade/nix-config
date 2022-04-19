95c1a1a7049fe82d65f02f60fabd54a622f61335
Nick Wade <me@ncwade.com>
2022-04-19 16:50:04 -0500

    Fix DNS configuration

--- services/dns.nix
+++ services/dns.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 let
-  adblockLocalZones = pkgs.stdenv.mkDerivation {
-    name = "unbound-zones-adblock";
+  stevenBlackList = pkgs.stdenv.mkDerivation {
+    name = "steven-black-adblock";
 
     src = (pkgs.fetchFromGitHub {
       owner = "StevenBlack";
       repo = "hosts";
       rev = "3.9.62";
-      sha256 = "01g6pc9s1ah2w1cbf6bvi424762hkbpbgja9585a0w99cq0n6bxv";
+      sha256 = "1xgxia1vrkrax80ab24a90ync5zps0y7zwj2v1689kzx43dakfn0";
     } + "/hosts");
 
     phases = [ "installPhase" ];
@@ -15,21 +15,45 @@ let
     installPhase = ''
       ${pkgs.gawk}/bin/awk '{sub(/\r$/,"")} {sub(/^127\.0\.0\.1/,"0.0.0.0")} BEGIN { OFS = "" } NF == 2 && $1 == "0.0.0.0" { print "local-zone: \"", $2, "\" static"}' $src | tr '[:upper:]' '[:lower:]' | sort -u >  $out
     '';
-
   };
 in {
   networking.firewall.allowedUDPPorts = [ 53 ];
   networking.firewall.allowedTCPPorts = [ 53 ];
   services.unbound = {
     enable = true;
-    allowedAccess = [ "127.0.0.0/24" "192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24" ];
-    interfaces = [ "0.0.0.0" ];
-    forwardAddresses = [ "1.0.0.1@853#cloudflare-dns.com" "1.1.1.1@853#cloudflare-dns.com" ];
-    extraConfig = ''
-        so-reuseport: yes
-        tls-cert-bundle: /etc/ssl/certs/ca-certificates.crt
-        tls-upstream: yes
-        include: "${adblockLocalZones}"
-        '';
+    settings = {
+      server = {
+        include = [
+          "${stevenBlackList}"
+        ];
+        access-control = [
+          "127.0.0.0/24 allow"
+          "192.168.1.0/24 allow"
+          "192.168.2.0/24 allow"
+          "192.168.3.0/24 allow"
+        ];
+        interface = [
+          "0.0.0.0"
+        ];
+        local-zone = [
+          "home.ncw. transparent"
+        ];
+        local-data = [
+          ''"nas1.home.ncw A 192.168.3.9"''
+          ''"rock1.home.ncw A 192.168.3.12"''
+          ''"rock2.home.ncw A 192.168.3.13"''
+        ];
+      };
+      forward-zone = [
+        {
+          name = ".";
+          forward-tls-upstream = "yes";
+          forward-addr = [
+          "1.1.1.1@853#cloudflare-dns.com"
+          "1.0.0.1@853#cloudflare-dns.com"
+        ];
+      }
+    ];
   };
+};
 }
